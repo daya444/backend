e1278ac2a8803ac3999a912ad36a43c5d0837d95
daya444 <dayavelusamy@gmail.com>
2025-05-12 14:32:12 +0530

    changed end point

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 // CORS configuration
 app.use(cors({
   origin: [
-    "https://66fc420df914aaa5ff72f43f--gbt-turbo.netlify.app",
+    "https://gbt-turbo.netlify.app",
     "http://localhost:5173"
   ],
   credentials: true, // Allow cookies
